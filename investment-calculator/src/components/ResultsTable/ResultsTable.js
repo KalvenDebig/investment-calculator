05988ec7bbfea555f881bb68734fc3f6104b7da1
Kalven Shen <kalvens@CKT.local>
2023-10-03 09:35:49 -0700

    feat: Split app intp components

---
+++ investment-calculator/src/components/ResultsTable/ResultsTable.js
@@ -0,0 +1,26 @@
+const ResultsTable = () => {
+  return (
+    <table className="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <th>YEAR NUMBER</th>
+          <th>TOTAL SAVINGS END OF YEAR</th>
+          <th>INTEREST GAINED IN YEAR</th>
+          <th>TOTAL INTEREST GAINED</th>
+          <th>TOTAL INVESTED CAPITAL</th>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default ResultsTable;
