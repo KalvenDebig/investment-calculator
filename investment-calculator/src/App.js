46d4a1fd0d5747c11ac198d866a2d2a9103f21ab
Sijie Shen <shen.sij@northeastern.edu>
2023-10-03 01:53:34 -0700

    feat: initialization

--- investment-calculator/src/App.js
+++ investment-calculator/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/investment-calculator-logo.png";
 
 function App() {
+  const calculateHandler = (userInput) => {
+    // Should be triggered when form is submitted
+    // You might not directly want to bind it to the submit event
+
+    const yearlyData = []; // per-year results
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    // The below code calculates yearly results (total savings, interest, etc
+    for (let i = 0; i < duration; i ++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution
+      });
+    }
+
+    // do something with yearlyData
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
       </header>
+
+      <form className="form">
+        <div className="input-group">
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input type="number" id="current-savings" />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Contribution ($)</label>
+            <input type="number" id="yearly-contribution" />
+          </p>
+        </div>
+
+        <div className="input-group">
+          <p>
+            <label htmlFor="expected-return">Expected Interest ($, per year)</label>
+            <input type="number" id="expected-return" />
+          </p>
+          <p>
+            <label htmlFor"duration">Investment Duration (years)</label>
+            <input type="number" id="duration" />
+          </p>
+        </div>
+        <p className="actions">
+          <button type"reset" className="buttonAlt">Reset</button>
+          <button type="submit" className="button">Calculate</button>
+        </p>
+      </form>
+      {/* todo: Show below table conditionally (only once result data is available) */}
+      {/* todo: Show fallback text if no data is available */}
+
+      <table className="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>YEAR NUMBER</th>
+            <th>TOTAL SAVINGS END OF YEAR</th>
+            <th>INTEREST GAINED IN YEAR</th>
+            <th>TOTAL INTEREST GAINED</th>
+            <th>TOTAL INVESTED CAPITAL</th>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
